b852a8b9d59789fd714e1119b02060add53468f6
youssefares <youssefares@ymail.com>
2016-06-20 17:01:54 +0200

    home page built & styled

---
+++ helpers.php
@@ -0,0 +1,10 @@
+<?php
+ function render($template, $data = array()){
+   $path = __DIR__.'/templates/'.$template.'.php';
+   if(file_exists($path)){
+     extract($data);
+     require($path);
+   }
+ }
+
+?>
